f249c6599be28240a309ae302bee9e12ab6b20c5
mohamed.gad <mohamed.gad2622@gmail.com>
2020-04-10 23:25:21 +0200

    update travis

--- src/index.test.js
+++ src/index.test.js
@@ -14,7 +14,6 @@ describe("index.html", () => {
     // get reference of index.html file and put it in memory
     const index = fs.readFileSync("./src/index.html", "utf-8");
     const dom = new JSDOM(index).window;
-
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
     expect(h1.innerHTML).to.equal("hello");
   });
